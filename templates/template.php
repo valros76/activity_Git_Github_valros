03d1bd617f3e03f30d22b18c61760688b89d6a55
valros76 <valerian.dufrene@gmail.com>
2018-07-21 22:10:22 +0200

    Ajout du template.php

---
+++ templates/template.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8"/>
+		<link rel="stylesheet" href="<?= $locateCss ?>"/>
+		<title><?= $title ?></title>
+	<head>
+	<body>
+		<div id="container">
+			<header id="head">
+				<?= $header ?>
+			</header>
+
+			<section id="content">
+				<?= $content ?>
+			</section>
+		</div>
+	</body>
+</html>
